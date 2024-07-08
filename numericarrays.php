e058783dc98f4844e1ba81d83579241a219fe662
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-08 22:37:50 +0200

    Add notes on numeric arrays

---
+++ numericarrays.php
@@ -0,0 +1,21 @@
+<?php
+
+// Numeric arrays
+// Key - value pairs. In numeric - the key is the number
+
+$first_names = array("Lena", "John", "Eve", "Steve", "Mary");
+echo $first_names[0];
+
+$shop = "Biedronka";
+$messed_up_array = array("Lena", "John", "41", "Steve", $shop);
+echo "<br />";
+echo "<br />";
+echo $messed_up_array [2];
+echo "<br />";
+echo $messed_up_array [4];
+echo "<br />";
+echo "<br />";
+
+// multidimentional array
+$names = array("Kovalsky", "Maliniak", "Doe", $first_names);
+echo $names[3][1];
