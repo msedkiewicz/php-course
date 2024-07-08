2bfd2ea22b3488ac03222213a797fd41ea35349b
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-08 21:59:30 +0200

    Add if else notes

---
+++ ifelse.php
@@ -0,0 +1,15 @@
+<?php
+
+// If else statements
+
+$first_name = "John";
+
+/* if (something is true) {
+do something;
+}*/
+
+if ($first_name == "John") {
+    echo "Hello John! How are you?";
+} else {
+    echo "What a lovely day!";
+}
