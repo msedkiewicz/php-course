585c38351ec53bae6277cc9867240cab15ee00fa
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-09 16:03:45 +0200

    Random game

--- index.php
+++ index.php
@@ -11,4 +11,20 @@ echo "<br />";
 echo "<br />";
 
 $names = array("John", "Steve", "Mary", "Joshua", "Ben", "Xavier");
-echo $names[mt_rand(0, count($names) - 1)];
+
+$random = mt_rand(0, count($names) - 1);
+echo $names[$random];
+
+// Random game
+echo "<br />";
+echo "<br />";
+
+echo "Guess a number! <br />";
+$num = 4;
+$random_number = mt_rand(0,10);
+
+if ($num == $random_number) {
+    echo "You win! $num";
+} else {
+    echo "Try again. $random_number";
+}
