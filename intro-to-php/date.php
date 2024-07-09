b924b67e19fd538a395ffe7812e2ee0442097a07
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-09 20:14:37 +0200

    Add notes on showing date in PHP

---
+++ intro-to-php/date.php
@@ -0,0 +1,16 @@
+<?php
+
+// Date function
+
+echo date('Y');
+echo "<br />";
+echo date('y');
+echo "<br />";
+echo date('l, jS \of F, Y ');
+echo "<br />";
+echo "Today is " . date('l');
+
+$today = date('Y');
+
+echo "<br />";
+echo "Copyright (c) $today - all right reserved";
--- intro-to-php/random.php
+++ intro-to-php/random.php
@@ -1,1 +1,30 @@
 <?php
+
+echo rand();
+echo "<br />";
+echo rand(0,100);
+echo "<br />";
+echo "<br />";
+// better algorithm
+echo mt_rand(0,10);
+echo "<br />";
+echo "<br />";
+
+$names = array("John", "Steve", "Mary", "Joshua", "Ben", "Xavier");
+
+$random = mt_rand(0, count($names) - 1);
+echo $names[$random];
+
+// Random game
+echo "<br />";
+echo "<br />";
+
+echo "Guess a number! <br />";
+$num = 4;
+$random_number = mt_rand(0,10);
+
+if ($num == $random_number) {
+    echo "You win! $num";
+} else {
+    echo "Try again. $random_number";
+}
